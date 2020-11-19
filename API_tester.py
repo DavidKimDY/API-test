c76ee14c6673d8d139c6f64b9cb6f67203d330a2
DAEYEOP <kdaeyoub@gmail.com>
2020-11-19 17:41:47 +0900

    tester

--- API_tester.py
+++ API_tester.py
@@ -3,30 +3,55 @@ import os
 import requests
 
 
-API_FOLDER_PATH = '/Users/daeyeop/Work/API test'
-ENTITY = '/엔티티 분석'
-DEPENDENCY = '/의존 파싱'
-MORPHEME = '/형태소 분석'
-TEXT = '/Text'
-
-
 class Tester():
-    def __init__(self, api_key):
-        self.mor_url = 'https://api.x.core.today/a75a68b0b-f859-4bfa-a487-fa12f1a0bb4a/'
-        self.ent_url = 'http://api.x.core.today/ab7eb939d-9d08-41cc-be2f-226065331e7d/'
-        self.dep_url = 'http://api.x.core.today/aca29268b-b75d-4009-8db5-22d756a5d36b/'
+    def __init__(self, api_key, kind=None):
+        assert kind is not None, 'Tester.__init__ expected a key argument, one of [entity, morpheme, dependency]'
+        assert kind in ['entity', 'morpheme', 'dependency'], 'one of [entity, morpheme, dependency], not {}'.format(kind)
+        self.morpheme_url = 'https://api.x.core.today/a75a68b0b-f859-4bfa-a487-fa12f1a0bb4a/'
+        self.entity_url = 'http://api.x.core.today/ab7eb939d-9d08-41cc-be2f-226065331e7d/'
+        self.dependency_url = 'http://api.x.core.today/aca29268b-b75d-4009-8db5-22d756a5d36b/'
+        self.url_dict = {
+                        'entity': self.entity_url,
+                        'morpheme': self.morpheme_url,
+                        'dependency': self.dependency_url
+                         }
         self.__api_key = api_key
-        self.text = ''
-        self.header = {
-            'api-key' : self.__api_key
-        }
-        self.params = {
-            'text': self.text
-        }
+        self.kind = kind
+        self.load_path = ''
+        self.header = {'api-key': self.__api_key}
+        self.params = {'text': ''}
 
-    def convert_to_entity(self, text_file: str):
-        assert os.path.isfile(text_file), 'Not found file : {}'.format(text_file)
-        with open(text_file, 'r', encoding='utf-8') as f:
-            self.text = f.read(f)
-        converted = requests.get(self.ent_url, headers=self.header, params=self.params)
-        print(json.loads(converted))
+    def _convert_and_save(self, text_file: str, url):
+        path = os.path.join(self.load_path + text_file)
+        assert os.path.isfile(path), 'Not found file : {}'.format(text_file)
+
+        with open(path, 'r', encoding='utf-8') as f:
+            self.params['text'] = f.read()
+
+        converted = requests.get(url, headers=self.header, params=self.params)
+        json_data = json.loads(converted.text)
+        self.save(json_data, text_file)
+
+    def convert(self, text_file: str):
+        url = self.url_dict[self.kind]
+        self._convert_and_save(text_file, url)
+
+    def set_text_path(self, load_path):
+        self.load_path = load_path
+
+    def save(self, json_data, text_file):
+        name = self.get_file_name(text_file)
+        if not os.path.isdir(self.kind):
+            os.mkdir(self.kind)
+
+        save_file_name = self.kind + '_' + name + '.json'
+        save_path = os.path.join(self.kind, save_file_name)
+
+        with open(save_path, 'w', encoding='utf-8') as f:
+            json.dump(json_data, f, ensure_ascii=False, indent='\t')
+
+    def get_file_name(self, text_file: str):
+        name = text_file.split('.')[0]
+        return name
+
+
--- tester.py
+++ tester.py
@@ -4,6 +4,10 @@ from API_tester import Tester
 
 MORPHEME_KEY = '14ff499053fdc7952f20948f33490e27'
 DEPENDENCY_KEY = '0398cc05b56c289b4f506c03f51da339'
+ENTITY_KEY = 'ef35bfb1abb5f4b6c2ac8c00a2203f2c'
 
-mor_tester = Tester(MORPHEME_KEY)
-mor_tester.convert_to_entity('1.txt')
+tester = Tester(MORPHEME_KEY, 'dependency')
+tester.set_text_path('/Users/daeyeop/Work/API test/text/')
+tester.convert('1.txt')
+
+
