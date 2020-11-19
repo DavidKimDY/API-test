4601f3d5e1db5af8b9081c4e668d7a5377a5e48d
DAEYEOP <kdaeyoub@gmail.com>
2020-11-19 13:43:36 +0900

    all

--- API tester.py
+++ API tester.py
@@ -1,4 +1,5 @@
 import json
+import requests
 
 API_FOLDER_PATH = '/Users/daeyeop/Work/API test'
 ENTITY = '/엔티티 분석'
@@ -7,13 +8,28 @@ MORPHEME = '/형태소 분석'
 TEXT = '/Text'
 
 
-class TestEntity():
+headers = {
+    "api-key": "5012f035sfj93ka923dk8s2fc5ce6"
+}
+params = {
+    "text": "안녕하세요 좋은 하루 입니다"
+}
+api_url = "https://api.x.core.today/a75a68b0b-f859-4bfa-a487-fa12f1a0bb4a/"
+r = requests.get(api_url, headers=headers, params=params)
+print(json.loads(r.text))
+
+
+class Tester():
     pass
 
 
-class TestMorpheme():
+class TestEntity(Tester):
     pass
 
 
-class TestDependency():
+class TestMorpheme(Tester):
+    pass
+
+
+class TestDependency(Tester):
     pass
