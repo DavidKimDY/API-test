f4ea66ffcd9fd22f048923b3221c5bdd5e61e00f
DAEYEOP <kdaeyoub@gmail.com>
2020-11-19 12:17:33 +0900

    init

---
+++ API tester.py
@@ -0,0 +1,19 @@
+import json
+
+API_FOLDER_PATH = '/Users/daeyeop/Work/API test'
+ENTITY = '/엔티티 분석'
+DEPENDENCY = '/의존 파싱'
+MORPHEME = '/형태소 분석'
+TEXT = '/Text'
+
+
+class TestEntity():
+    pass
+
+
+class TestMorpheme():
+    pass
+
+
+class TestDependency():
+    pass
